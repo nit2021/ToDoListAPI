13ec48977a74142fa850d33cbb1e7e97b717643a
Nit2021 <nitin.jain03@nagarro.com>
2021-11-01 20:48:55 +0530

    Code Migrations

---
+++ Migrations/20211030171257_InitialCreate.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoListAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(maxLength: 50, nullable: false),
+                    Password = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoItem",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(maxLength: 50, nullable: false),
+                    Owner = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoItem", x => x.ItemId);
+                    table.ForeignKey(
+                        name: "FK_ToDoItem_User_Owner",
+                        column: x => x.Owner,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Label",
+                columns: table => new
+                {
+                    LabelId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(maxLength: 50, nullable: false),
+                    ItemID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Label", x => x.LabelId);
+                    table.ForeignKey(
+                        name: "FK_Label_ToDoItem_ItemID",
+                        column: x => x.ItemID,
+                        principalTable: "ToDoItem",
+                        principalColumn: "ItemId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "UserId", "Password", "UserName" },
+                values: new object[] { 101, "Pa$$w0rd", "Admin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Label_ItemID",
+                table: "Label",
+                column: "ItemID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoItem_Owner",
+                table: "ToDoItem",
+                column: "Owner");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Label");
+
+            migrationBuilder.DropTable(
+                name: "ToDoItem");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
