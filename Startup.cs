cd8c2e1af25287a85fc0b2807033e69c823af7f2
Nit2021 <nitin.jain03@nagarro.com>
2021-11-01 19:55:00 +0530

    init

---
+++ Startup.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BasicAuth.API;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using ToDoListAPI.DAL;
+using ToDoListAPI.Models;
+using ToDoListAPI.Services;
+
+namespace ToDoListAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ToDoContext>(optionsAction=>
+            optionsAction.UseSqlServer(Configuration.GetConnectionString("ToDoContext")));
+            //services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddControllers();
+
+            #region ConfigureSwagger
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title="ToDoList Demo API",
+                    Description="Demo for showing swagger",
+                    Version="v1"
+                });
+                options.AddSecurityDefinition("basic", new OpenApiSecurityScheme  
+                {  
+                    Name = "Authorization",  
+                    Type = SecuritySchemeType.Http,  
+                    Scheme = "basic",  
+                    In = ParameterLocation.Header,  
+                    Description = "Basic Authorization header using the Bearer scheme."  
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement  
+                {  
+                    {  
+                          new OpenApiSecurityScheme  
+                            {  
+                                Reference = new OpenApiReference  
+                                {  
+                                    Type = ReferenceType.SecurityScheme,  
+                                    Id = "basic"  
+                                }  
+                            },  
+                            new string[] {}  
+                    }  
+                });
+            });
+            #endregion
+
+            services.AddAuthentication("BasicAuthentication")  
+            .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);  
+  
+            services.AddScoped<IUserService, UserService>();  
+            services.AddScoped<IToDoService, ToDoService>();  
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();  
+                app.UseSwaggerUI(c =>  
+                {  
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Test1 Api v1");  
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMiddleware<RequestResponseLoggingMiddleware>();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
