412b7be2a8cc309654a35384290827c41943ff74
Nit2021 <nitin.jain03@nagarro.com>
2021-11-15 11:52:09 +0530

    Project Restructured

--- Services/IUserService.cs
+++
@@ -1,8 +0,0 @@
-namespace ToDoListAPI.Services
-{
-    public interface IUserService
-    {
-        public int userId { get; set; }
-        bool ValidateCredentials(string username, string password);
-    }
-}
