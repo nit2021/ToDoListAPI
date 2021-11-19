5ce6e28b2a22f3c3ef114549bd4014dfe48ac36b
Nit2021 <nitin.jain03@nagarro.com>
2021-11-19 17:26:17 +0530

    Added encryption logic to password

--- ToDoDAL/DAL/ModelBuilderExtensions.cs
+++ ToDoDAL/DAL/ModelBuilderExtensions.cs
@@ -6,8 +6,8 @@ namespace ToDoAPI.DAL
     {
         public static void Seed(this ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Users>().HasData(new Users { UserId = 101, UserName = "Admin", Password = "Pa$$w0rd" });
-            modelBuilder.Entity<Users>().HasData(new Users { UserId = 102, UserName = "Standard", Password = "Pa$$w0rd" });
+            modelBuilder.Entity<Users>().HasData(new Users { UserId = 101, UserName = "Admin", Password = Cryptography.Encrypt("Pa$$w0rd") });
+            modelBuilder.Entity<Users>().HasData(new Users { UserId = 102, UserName = "Standard", Password = Cryptography.Encrypt("Pa$$w0rd2") });
             modelBuilder.Entity<ToDoList>().HasData(new ToDoList { ListId = 201, Description = "ListItem1", OwnerID = 101, CreatedDate = System.DateTime.UtcNow });
             modelBuilder.Entity<ToDoItem>().HasData(new ToDoItem { ItemId = 301, Description = "Item1", ToDoListID = 201, CreatedDate = System.DateTime.UtcNow });
             modelBuilder.Entity<Label>().HasData(new Label { LabelId = 401, Description = "Label1", ToDoItemID = 301, ToDoListID = 201 });
