02380580172dff4b1a4092810bfc4859f72a9fc6
Nit2021 <nitin.jain03@nagarro.com>
2021-11-22 18:24:32 +0530

    Handled null reference exception

--- ToDo.API/API/BasicAuthenticationHandler.cs
+++ ToDo.API/API/BasicAuthenticationHandler.cs
@@ -12,52 +12,55 @@ using System.Threading.Tasks;
 
 namespace ToDoListAPI.ToDoAPI.BasicAuth
 {
-    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>  
-    {  
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
         #region Property  
-        readonly IUserService _userService;  
-        #endregion  
- 
+        readonly IUserService _userService;
+        #endregion
+
         #region Constructor  
-        public BasicAuthenticationHandler(IUserService userService,  
-            IOptionsMonitor<AuthenticationSchemeOptions> options,  
-            ILoggerFactory logger,  
-            UrlEncoder encoder,  
-            ISystemClock clock)  
-            : base(options, logger, encoder, clock)  
-        {  
-            _userService = userService;  
-        }  
-        #endregion  
-  
-        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()  
-        {  
-            string username = null;  
-            try  
-            {  
-                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);  
-                var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Parameter)).Split(':');  
-                username = credentials.FirstOrDefault();  
-                var password = credentials.LastOrDefault();  
-  
+        public BasicAuthenticationHandler(IUserService userService,
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+            _userService = userService;
+        }
+        #endregion
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            string username = null;
+            try
+            {
+                var header = Request.Headers.FirstOrDefault(h => h.Key.Equals("Authorization"));
+                if (header.Value.Count == 0)
+                    return AuthenticateResult.Fail($"Authentication failed: Invalid credentials");
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Parameter)).Split(':');
+                username = credentials.FirstOrDefault();
+                var password = credentials.LastOrDefault();
+
                 if (!_userService.ValidateCredentials(username, password))
-                return AuthenticateResult.Fail($"Authentication failed: Invalid credentials");    
-                    //throw new ArgumentException("Invalid credentials");  
-            }  
-            catch (Exception ex)  
-            {  
-                return AuthenticateResult.Fail($"Authentication failed: {ex.Message}");  
-            }  
-  
-            var claims = new[] {  
-                new Claim(ClaimTypes.Name, username)  
-            };  
-            var identity = new ClaimsIdentity(claims, Scheme.Name);  
-            var principal = new ClaimsPrincipal(identity);  
-            var ticket = new AuthenticationTicket(principal, Scheme.Name);  
-  
-            return AuthenticateResult.Success(ticket);  
-        }  
-  
-    }  
+                    return AuthenticateResult.Fail($"Authentication failed: Invalid credentials");
+                //throw new ArgumentException("Invalid credentials");  
+            }
+            catch (Exception ex)
+            {
+                return AuthenticateResult.Fail($"Authentication failed: {ex.Message}");
+            }
+
+            var claims = new[] {
+                new Claim(ClaimTypes.Name, username)
+            };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+
+    }
 }
